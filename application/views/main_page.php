f7c25802df4efaa3c4e8f179b151dd8dad4def24
Andrea Suárez <andreamendoza130@gmail.com>
2021-05-19 12:21:10 -0500

    Tercer commit

--- application/views/main_page.php
+++ application/views/main_page.php
@@ -16,13 +16,18 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 </head>
 <body>
 	<!-- Navigation -->
-	<nav class="navbar navbar-dark bg-dark">
+	<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
 		<div class="container-fluid">
-			<a class="navbar-brand">Navbar</a>
-			<form class="d-flex">
-				<input type="search" class="form-control me-2" placeholder="Buscar">
-				<button class="btn btn-outline-success" type="submit">Buscar</button>
-			</form>
+			<a class="navbar-brand">
+				<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwgbUlG1ZDqXC02eza0_hXdIqmeWg2Xb0Bj1EItxOcFgTct4hN5_Q6mALIvA5m3H_fgl0&usqp=CAU" alt="" width="50" height="44">
+			</a>
+			<div class="collapse navbar-collapse" id="navbarNav">
+				<ul class="navbar-nav">
+					<li class="nav-item">
+						<a class="nav-link active" aria-current="page" href="#">Directorio</a>
+					</li>
+				</ul>
+			</div>
 		</div>
 	</nav>
 	<!-- /Navigation -->
@@ -32,26 +37,80 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 		</div>
 		<div class="row">
 			<div class="col-8">
-				Contenido
+				<div class="row" style="margin-bottom: 15px">
+					<div class="col-sm-6">
+						<div class="card text-white bg-secondary">
+							<div class="card-header">
+								Título
+							</div>
+							<div class="card-body text-dark bg-light">
+								<p class="card-text">Contenido</p>
+								<a href="#" class="card-link">Link</a>
+							</div>
+						</div>
+					</div>
+					<div class="col-sm-6">
+						<div class="card text-white bg-secondary">
+							<div class="card-header">
+								Título 2
+							</div>
+							<div class="card-body text-dark bg-light">
+								<p class="card-text">Contenido</p>
+								<a href="#" class="card-link">Link 2</a>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-sm-6">
+						<div class="card text-white bg-secondary">
+							<div class="card-header">
+								Título3
+							</div>
+							<div class="card-body text-dark bg-light">
+								<p class="card-text">Contenido</p>
+								<a href="#" class="card-link">Link</a>
+							</div>
+						</div>
+					</div>
+					<div class="col-sm-6">
+						<div class="card text-white bg-secondary">
+							<div class="card-header">
+								Título 4
+							</div>
+							<div class="card-body text-dark bg-light">
+								<p class="card-text">Contenido</p>
+								<a href="#" class="card-link">Link 2</a>
+							</div>
+						</div>
+					</div>
+				</div>
 			</div>
 			<div class="col">
-				<form>
-					<div class="mb-3">
-						<label for="username" class="form-label">Username:</label>
-						<input type="text" class="form-control" name="user" id="user" placeholder="@ejemplo" required>
-						<div class="invalid-feedback">
-							Por favor ingresa el usuario.
-						</div>
+				<div class="card text-white bg-dark mb-3">
+					<div class="card-header">
+						Login
 					</div>
-					<div class="mb-3">
-						<label for="password" class="form-label">Contraseña:</label>
-						<input type="password" class="form-control" name="pass" id="pass" placeholder="@Contraseña" required>
-						<div class="invalid-feedback">
-							Contraseña incorrecta.
-						</div>
+					<div class="card-body text-dark bg-light">
+						<form>
+							<div class="mb-3">
+								<label for="username" class="form-label">Username:</label>
+								<input type="text" class="form-control" name="user" id="user" placeholder="@ejemplo" required>
+								<div class="invalid-feedback">
+									Por favor ingresa el usuario.
+								</div>
+							</div>
+							<div class="mb-3">
+								<label for="password" class="form-label">Contraseña:</label>
+								<input type="password" class="form-control" name="pass" id="pass" placeholder="@Contraseña" required>
+								<div class="invalid-feedback">
+									Contraseña incorrecta.
+								</div>
+							</div>
+							<button class="btn btn-primary" type="submit">Iniciar sesión</button>
+						</form>
 					</div>
-					<button class="btn btn-primary" type="submit">Iniciar sesión</button>
-				</form>
+				</div>
 			</div>
 		</div>
 	</div>
